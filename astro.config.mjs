7ee2f8611e142868a1553b997b3a597c1d9bed5d
Adreon <sachioramzy12@gmail.com>
2024-11-21 14:51:01 +0700

    Configuring Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  site: "https://Adreon19.github.io",
+  base: "/New-Portofolio",
 });
